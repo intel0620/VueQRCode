18a100a886933f327826f746299f149b29c9466f
Steven.shih <intel0620@gmail.com>
2020-04-24 08:30:40 +0800

    加入專案檔案。

---
+++ VueQRCode/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VueQRCode.Startup))]
+namespace VueQRCode
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
