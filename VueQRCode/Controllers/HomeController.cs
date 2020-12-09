4906062713356acfb897f9a1b687d007f676eaae
Steven.shih <intel0620@gmail.com>
2020-12-09 17:22:54 +0800

    #20201209ParseQueryString

--- VueQRCode/Controllers/HomeController.cs
+++ VueQRCode/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,10 +17,40 @@ namespace VueQRCode.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewData["myurl"] = "";
             return View();
         }
+
+        [HttpPost]
+        public ActionResult About(string HTTPURL)
+        {
+            NameValueCollection qscoll = HttpUtility.ParseQueryString(HTTPURL);
+
+            var Tag = string.IsNullOrEmpty(qscoll["tag"]) ? "" : qscoll["tag"];
+            //var Tc = string.IsNullOrEmpty(Request.QueryString["tc"]) ? "" : Request.QueryString["tc"];
+            //var Page = string.IsNullOrEmpty(Request.QueryString["p"]) ? "" : Request.QueryString["p"];
+            // Iterate through the collection.
+            //StringBuilder sb = new StringBuilder();
+            //foreach (String s in qscoll.AllKeys)
+            //{
+            //    sb.Append(s + " - " + qscoll[s] + "<br />");
+            //}
+
+            if (Tag != "")
+            {
+                    ViewData["myurl"] ="產品序號 : "+  Tag.ToString() + "<br />建議定價: 100 <br /> 代理商: ABCD <br /> 經銷商: EFGH <br /> 授權區域: 新北市、台北市、宜蘭縣";
+
+
+            }
+            else
+            {
+                ViewData["myurl"] = "";
+            }
+           
+            ViewBag.Message = "ok";
+            return View();
+        }
+
 
         public ActionResult Contact()
         {
